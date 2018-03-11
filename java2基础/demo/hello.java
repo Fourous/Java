f893b497c5c01115da8c38e99c5ddf6d39e9aff5
2120161650 <2120161650>
2018-03-11 23:17:15 +0800

    first commit java

---
+++ java2基础/demo/hello.java
@@ -0,0 +1,13 @@
+import java.util.*  //应用java 库，这里库包含许多，＊代表所有
+//每个java 文件中会应用特定类２导入，java.lang
+public static void main(String [] args){
+	/*
+	static 静态方法，因为不用创建对象就可以直接应用，所有一般作为ｍａｉｎ函数的入口
+	static 数据变量不可改变，并且可以直接用内名直接应用，方法可以重载
+	也可以创建对象，会公用一个存储区域，一般内名进行应用加快速度
+	*/
+	System.out.println("Hello.world!");
+	
+	
+	}
+
